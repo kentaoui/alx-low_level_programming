5a94b8388fcc42d89d21969135a76fc4d48cf2ad
kentaoui <chaimaeelkentaoui941@gmail.com>
2023-07-24 19:06:39 -0700

    d

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,16 +5,12 @@
  * @s: Input string
  * Return: String in reverse
  */
-
 void rev_string(char *s)
 {
 	int l, i;
 	char tmp;
 
-	/*find string length without null char*/
 	for (l = 0; s[l] != '\0'; ++l)
-
-	/*swap the string by looping to half the string*/
 	for (i = 0; i < l / 2; i++)
 	{
 		tmp = s[i];
