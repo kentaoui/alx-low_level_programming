3dff4c9df679dfdd82e84741cb724ff905867956
kentaoui <chaimaeelkentaoui941@gmail.com>
2023-07-24 19:17:25 -0700

    rev-string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,16 +5,20 @@
  * @s: Input string
  * Return: String in reverse
  */
+
 void rev_string(char *s)
 {
-	int l, i;
-	char tmp;
+	char rev = s[0];
+	int counter = 0;
+	int i;
 
-	for (l = 0; s[l] != '\0'; ++l)
-	for (i = 0; i < l / 2; i++)
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
 	{
-		tmp = s[i];
-		s[i] = s[l - 1 - i];
-		s[l - 1 - i] = tmp;
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
 }
